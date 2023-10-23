f2976f891d5d936c91698a1bb892bb19a65f0e12
Rom341 <j005@mail.ru>
2023-10-23 22:11:30 +1000

    change code cook_book_1

--- cook_book_1.py
+++ cook_book_1.py
@@ -1,9 +1,9 @@
 from pprint import pprint
-import json as json
+# import json as json
 from pprint import pformat
 
 
-# def make_cook_book():
+
 with open('recipes.txt', encoding='UTF-8') as f:
     cook_book = {}
     for line in f.read().split("\n\n"):
@@ -24,11 +24,9 @@ with open('recipes.txt', encoding='UTF-8') as f:
             # print(cook_lst)
             # print(' ')
         cook_book[dish_name] = cook_lst
-    # return cook_book
-    # format_cook_book = pformat(cook_book)
-    # print(f"cook_book = ")
+
+    print(f"cook_book =")
     pprint(cook_book, width=100)
-    # pprint(format_cook_book)
     print(' ')
 
 
@@ -45,32 +43,3 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-
-# cook_book = {
-#   'Омлет': [
-#     {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-#     {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-#     {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-#     ],
-#   'Утка по-пекински': [
-#     {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-#     {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-#     {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-#     {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-#     ],
-#   'Запеченный картофель': [
-#     {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-#     {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-#     {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-#     ]
-#   }
-
-
-# {
-#   'Картофель': {'measure': 'кг', 'quantity': 2},
-#   'Молоко': {'measure': 'мл', 'quantity': 200},
-#   'Помидор': {'measure': 'шт', 'quantity': 4},
-#   'Сыр гауда': {'measure': 'г', 'quantity': 200},
-#   'Яйцо': {'measure': 'шт', 'quantity': 4},
-#   'Чеснок': {'measure': 'зубч', 'quantity': 6}
-# }
